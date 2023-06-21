ec3900839bd7d5f37c9e9f2696a8b9d8ee4fbd3e
jacobi petrucciani <j@cobi.dev>
2023-06-20 21:02:24 -0400

    add meme

--- hosts/neverland/configuration.nix
+++ hosts/neverland/configuration.nix
@@ -38,6 +38,13 @@ in
 
   home-manager.users.jade = common.jade;
   networking.hostName = hostname;
+  nix = common.nix // {
+    nixPath = [
+      "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+      "nixos-config=/home/jade/cfg/hosts/${hostname}/configuration.nix"
+      "/nix/var/nix/profiles/per-user/root/channels"
+    ];
+  };
   nixpkgs.config.allowUnfree = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   programs.command-not-found.enable = false;
