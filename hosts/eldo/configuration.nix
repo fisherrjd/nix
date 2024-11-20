553e9d3b17ab57426ecb0aa2876c0cfcc866cf80
Jade (Robert) Fisher <fisherrjd@gmail.com>
2024-11-19 20:57:13 -0700

    cobi fucked up smh no ones suprised

--- hosts/eldo/configuration.nix
+++ hosts/eldo/configuration.nix
@@ -103,8 +103,6 @@ in
   # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
