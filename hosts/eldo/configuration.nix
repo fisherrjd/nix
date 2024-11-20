63bd6cbcfaf0249d8e1a2053832e1f9b0b787082
Jade (Robert) Fisher <fisherrjd@gmail.com>
2024-11-19 20:42:09 -0700

    add flake?

--- hosts/eldo/configuration.nix
+++ hosts/eldo/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, machine-name, ... }:
+{ config, flake, pkgs, machine-name, ... }:
 
 let
   hostname = "eldo";
@@ -14,7 +14,6 @@ in
       "${common.home-manager}/nixos"
       ./hardware-configuration.nix
     ];
-
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -140,8 +139,6 @@ in
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
-
 
   #begin jade fuckin around
   environment.variables = {
