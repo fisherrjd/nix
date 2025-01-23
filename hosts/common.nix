f8450f03dbb10655acf43fa678f3d7bce0d53448
jade fisher <fisherrjd@gmail.com>
2025-01-23 15:58:18 -0700

    syntax

--- hosts/common.nix
+++ hosts/common.nix
@@ -57,7 +57,7 @@ in
     neverland = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPLw7NAEfBzS9PzgS2CtFmtQ6dk5wq4BC3YXhkS4iT/j jade@neverland"; #home wsl on home desktop
     eldo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbNhnkhqLCDhVYXTQXxuVYkPHnWSBFFmunVSk5ETnZj jade@eldo"; # old pc gone nix server
     airbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILaWm9is28MRcO96v72pHvWQuZ+NiM0t3iFmC4mq3jsJ jade@airbook"; # m1 macbook air
-    workbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDMR5a3uP1lZndQ8BZhirgYwHwbZNdzeoLeAwdOnslZf jade@work" #m1pro work CHARTER
+    workbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDMR5a3uP1lZndQ8BZhirgYwHwbZNdzeoLeAwdOnslZf jade@work"; #m1pro work CHARTER
   };
   timeZone = "America/Denver";
 
