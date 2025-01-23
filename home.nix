69fe2314dcb08b68d1302fa40a1504c1eefe31ea
jade fisher <fisherrjd@gmail.com>
2025-01-23 16:27:33 -0700

    meme for user stuff?

--- home.nix
+++ home.nix
@@ -311,15 +311,27 @@ in
         '';
       in
       ''
-        User jade
-        PasswordAuthentication no
-        IdentitiesOnly yes
-        # secure stuff
-        Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
-        KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256
-        MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
-        HostKeyAlgorithms ssh-ed25519,rsa-sha2-256,rsa-sha2-512
-        ${optionalString isDarwin mac_meme}
+        Host airbook
+          User jade
+          PasswordAuthentication no
+          IdentitiesOnly yes
+          # secure stuff
+          Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+          KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256
+          MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
+          HostKeyAlgorithms ssh-ed25519,rsa-sha2-256,rsa-sha2-512
+          ${optionalString isDarwin mac_meme}
+          
+        Host workbook
+          User P3175941
+          PasswordAuthentication no
+          IdentitiesOnly yes
+          # secure stuff
+          Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+          KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256
+          MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
+          HostKeyAlgorithms ssh-ed25519,rsa-sha2-256,rsa-sha2-512
+          ${optionalString isDarwin mac_meme}
       '';
   };
 
