1ff80c3f9a504cf84db45d6c427411a329fcf2bc
jade fisher <fisherrjd@gmail.com>
2025-01-24 14:13:38 -0700

    guh

--- home.nix
+++ home.nix
@@ -298,16 +298,23 @@ in
         '';
       in
       ''
-      Host 10.0.0.61
-        HostName workbook
-        User P3175941
-        PasswordAuthentication no
-        IdentitiesOnly yes
-        # secure stuff
-        Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
-        KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256
-        MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
-        HostKeyAlgorithms ssh-ed25519,rsa-sha2-256,rsa-sha2-512
+      User jade
+      PasswordAuthentication no
+      IdentitiesOnly yes
+      # secure stuff
+      Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+      KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256
+      MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
+      HostKeyAlgorithms ssh-ed25519,rsa-sha2-256,rsa-sha2-512
+
+      User P3175941
+      PasswordAuthentication no
+      IdentitiesOnly yes
+      # secure stuff
+      Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+      KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256
+      MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
+      HostKeyAlgorithms ssh-ed25519,rsa-sha2-256,rsa-sha2-512
       '';
   };
 
