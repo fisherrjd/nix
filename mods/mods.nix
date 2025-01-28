770879d4d065731a06391a0371eed023f16cb9ce
jade fisher <fisherrjd@gmail.com>
2025-01-28 16:38:17 -0700

    Revert "remove if on darwin"
    This reverts commit 3d984f65b4991e26fad4e14db1c697191a3bd920.

--- mods/mods.nix
+++ mods/mods.nix
@@ -21,10 +21,12 @@ rec {
     writeShellScriptBin "switch" ''
       profile=/nix/var/nix/profiles/system
       toplevel="$(nix build ${flags} --no-link --print-out-paths ~/cfg#darwinConfigurations.${host}.system)"
+      if [[ $(realpath "$profile") != "$toplevel" ]];then
         ${nvd}/bin/nvd diff "$profile" "$toplevel"
         sudo -H nix-env -p "$profile" --set "$toplevel"
         "$toplevel"/activate-user
         sudo "$toplevel"/activate
+      fi
     '';
   _hms = {
     default = ''
