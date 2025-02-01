d6695911a1b96cac971f200df2a1b53eef860118
jade fisher <fisherrjd@gmail.com>
2025-01-31 20:25:28 -0700

    remove docker

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
               specialArgs = { flake = self; machine-name = name; };
               modules = [
                 ./hosts/common_darwin.nix
-                ./hosts/modules/
+                ./hosts/modules/llama-server.nix
                 ./hosts/${name}/configuration.nix
               ];
             };
