3dc18f9cf72ec9973a2859d08a87ffcda9cd9ee3
jacobi petrucciani <8117202+jpetrucciani@users.noreply.github.com>
2025-02-07 15:26:57 -0500

    add nix-ld, cleanup

--- hosts/eldo/configuration.nix
+++ hosts/eldo/configuration.nix
@@ -96,8 +96,7 @@ in
     packages = with pkgs; [ ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
+  programs.nix-ld.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -106,36 +105,10 @@ in
     vscode
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
-  #begin jade fuckin around
-
-  #enable security sudo users from common
+  # begin jade fuckin around
   security.sudo = common.security.sudo;
 
   #define hostname env variable
@@ -143,7 +116,6 @@ in
     NIX_HOST = hostname;
   };
   networking.hostName = "eldo"; # Define your hostname.
-
 
   #TODO: Learn what this is doing??? 
   #TODO: I think its enabling home manager stuff from jade user in common 
