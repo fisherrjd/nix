e1deb42708cf9268ca6471fc2f7bf57bfb15029b
jade fisher <fisherrjd@gmail.com>
2025-02-20 16:05:38 -0700

    remove the bits again

--- hosts/neverland/configuration.nix
+++ hosts/neverland/configuration.nix
@@ -21,7 +21,6 @@ in
     enable = true;
     defaultUser = "jade";
     startMenuLaunchers = true;
-    nativeSystemd = true;
     wslConf.automount.root = "/mnt";
     # Enable native Docker support
     # docker-native.enable = true;
