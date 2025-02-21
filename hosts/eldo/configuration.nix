37c4f1e030f5305b4112e63346138afbb24164ff
jade fisher <fisherrjd@gmail.com>
2025-02-20 20:36:51 -0700

    remove random bits

--- hosts/eldo/configuration.nix
+++ hosts/eldo/configuration.nix
@@ -47,41 +47,6 @@ in
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jade = {
     isNormalUser = true;
@@ -105,7 +70,10 @@ in
     vscode
   ];
 
-  services.openssh.enable = true;
+  services =
+    {
+      openssh.enable = true;
+    };
   system.stateVersion = "24.05"; # Did you read the comment?
 
   # begin jade fuckin around
@@ -135,4 +103,10 @@ in
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
+
+  # sytemd.services.startup = {
+  #   description = "Random Startup Service thing";
+  #   after = [ "network.target" ];
+  #   restart = "always";
+  # };
 }
