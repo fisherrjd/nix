145d53807875c352e27f4d312d4ff962a9732a29
jade fisher <fisherrjd@gmail.com>
2025-02-25 16:15:47 -0700

    move some of bash

--- home.nix
+++ home.nix
@@ -26,7 +26,7 @@ in
   imports = [
     ./home_configurations/packages.nix
     ./home_configurations/cobi.nix
-    # ./home_configurations/bash.nix
+    ./home_configurations/bash.nix
     # Look more into these ex: optionalAttrs
     (pkgs.lib.optionalAttrs isLinux "${flake.inputs.vscode-server}/modules/vscode-server/home.nix")
   ];
@@ -79,25 +79,6 @@ in
     enable = true;
     historyFileSize = -1;
     historySize = -1;
-    shellAliases = {
-      ls = "ls --color=auto";
-      l = "lsd -lA --permission octal";
-      ll = "ls -ahlFG";
-      mkdir = "mkdir -pv";
-      fzfp = "${pkgs.fzf}/bin/fzf --preview 'bat --style=numbers --color=always {}'";
-      strip = ''${pkgs.gnused}/bin/sed -E 's#^\s+|\s+$##g' '';
-
-      # git
-      g = "git";
-      ga = "git add -A .";
-      cm = "git commit -m ";
-      test2 = "echo 'test'";
-
-      # misc
-      space = "du -Sh | sort -rh | head -10";
-      now = "date +%s";
-      uneek = "awk '!a[$0]++'";
-    };
     bashrcExtra =
       if isDarwin then ''
         export PATH="$PATH:${homeDirectory}/.nix-profile/bin"
--- home_configurations/bash.nix
+++ home_configurations/bash.nix
@@ -1,80 +1,29 @@
-{ pkgs, flake, lib, machine-name, homeDirectory, sessionVariables, ... }:
+{ pkgs, flake, lib, ... }:
 let
-  sessionVariables = "temp";
-  inherit (pkgs.hax) isDarwin isLinux isM1;
+  TEMP = "placeholder";
 
 in
 {
   programs.bash = {
-    inherit sessionVariables;
-    enable = true;
-    historyFileSize = -1;
-    historySize = -1;
-    shellAliases = { };
-    bashrcExtra =
-      if isDarwin then ''
-        export PATH="$PATH:${homeDirectory}/.nix-profile/bin"
-      '' else "";
-    initExtra = ''
-      HISTCONTROL=ignoreboth
-      set +h
-      export PATH="$PATH:$HOME/.bin/"
-      export PATH="$PATH:$HOME/.npm/bin/"
+    shellAliases = {
+      ls = "ls --color=auto";
+      l = "lsd -lA --permission octal";
+      ll = "ls -ahlFG";
+      mkdir = "mkdir -pv";
+      fzfp = "${pkgs.fzf}/bin/fzf --preview 'bat --style=numbers --color=always {}'";
+      strip = ''${pkgs.gnused}/bin/sed -E 's#^\s+|\s+$##g' '';
 
-      # asdf and base nix
-    '' + (if isM1 then ''
-      export CONFIGURE_OPTS="--build aarch64-apple-darwin20"
-    ''
-    else ""
-    ) + (
-      if isDarwin then ''
-        # add brew to path
-        brew_path="/opt/homebrew/bin/brew"
-        if [ -f /usr/local/bin/brew ]; then
-          brew_path="/usr/local/bin/brew"
-        fi
-        eval "$($brew_path shellenv)"
+      # git
+      g = "git";
+      ga = "git add -A .";
+      cm = "git commit -m ";
+      test2 = "echo 'test'";
 
-        # load asdf if its there
-        asdf_dir="$(brew --prefix asdf)"
-        [[ -e "$asdf_dir/asdf.sh" ]] && source "$asdf_dir/asdf.sh"
-        [[ -e "$asdf_dir/etc/bash_completion.d/asdf.bash" ]] && source "$asdf_dir/etc/bash_completion.d/asdf.bash"
-      '' else ''
-        [[ -e $HOME/.asdf/asdf.sh ]] && source $HOME/.asdf/asdf.sh
-        [[ -e $HOME/.asdf/completions/asdf.bash ]] && source $HOME/.asdf/completions/asdf.bash
-      ''
-    ) + ''
-      # additional aliases
-      [[ -e ~/.aliases ]] && source ~/.aliases
-
-      # bash completions
-      export XDG_DATA_DIRS="$HOME/.nix-profile/share:''${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
-      source ~/.nix-profile/etc/profile.d/bash_completion.sh
-      source ~/.nix-profile/share/bash-completion/completions/git
-      source ~/.nix-profile/share/bash-completion/completions/ssh
-      complete -o bashdefault -o default -o nospace -F __git_wrap__git_main g
-      # there are often duplicate path entries on non-nixos; remove them
-      NEWPATH=
-      OLDIFS=$IFS
-      IFS=:
-      for entry in $PATH;do
-        if [[ ! :$NEWPATH: == *:$entry:* ]];then
-          if [[ -z $NEWPATH ]];then
-            NEWPATH=$entry
-          else
-            NEWPATH=$NEWPATH:$entry
-          fi
-        fi
-      done
-
-      IFS=$OLDIFS
-      export PATH="$NEWPATH"
-      unset OLDIFS NEWPATH
-    '' + (if isLinux then ''
-      ${pkgs.figlet}/bin/figlet "$(hostname)" | ${pkgs.clolcat}/bin/clol  cat
-      echo
-    '' else "");
+      # misc
+      space = "du -Sh | sort -rh | head -10";
+      now = "date +%s";
+      uneek = "awk '!a[$0]++'";
+    };
   };
-
 }
 
