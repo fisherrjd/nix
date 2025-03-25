8ffaca101b9c7540cddac3ced63d9ef222af4fd6
Jade (P3175941) <C-Jade.Fisher@charter.com>
2025-03-25 14:08:06 -0600

    re add default.nix

---
+++ default.nix
@@ -0,0 +1,13 @@
+{ _compat ? import ./flake-compat.nix
+, nixpkgs ? _compat.inputs.nixpkgs
+, overlays ? [ ]
+, config ? { }
+, system ? builtins.currentSystem
+}:
+import nixpkgs {
+  inherit system;
+  overlays = [ (_: _: { nixpkgsRev = nixpkgs.rev; }) ] ++ (import ./overlays.nix) ++ overlays;
+  config = {
+    allowUnfree = true;
+  } // config;
+} 
