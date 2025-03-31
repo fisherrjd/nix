e548e98508e25b72bfc836b52925cb6e88fcfb63
jade fisher <fisherrjd@gmail.com>
2025-03-31 10:23:55 -0600

    add postgres service?

--- hosts/eldo/configuration.nix
+++ hosts/eldo/configuration.nix
@@ -106,5 +106,19 @@ in
   services =
     {
       openssh.enable = true;
+      postgresql = {
+        enable = true;
+        ensureDatabases = [ "litellm" ];
+        authentication = pkgs.lib.mkOverride 10 ''
+          #type database  DBuser  auth-method
+          local all       all     trust
+          host all all 127.0.0.1/32 trust
+          host all all 0.0.0.0/0 trust
+          host    all            postgres         127.0.0.1/32           md5
+          host    all            postgres         ::1/128                md5
+          host    litellm        postgres         0.0.0.0/0              md5
+        '';
+      };
     };
+
 }
