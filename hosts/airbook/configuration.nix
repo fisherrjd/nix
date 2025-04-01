4791a7ed534f9bafcfe48aeed074471631410191
jade fisher <fisherrjd@gmail.com>
2025-04-01 13:24:00 -0600

    update power management on airbook

--- hosts/airbook/configuration.nix
+++ hosts/airbook/configuration.nix
@@ -18,55 +18,61 @@ in
     "${common.home-manager}/nix-darwin"
   ];
 
-  power.sleep = {
-    computer = "never";
-    display = "never";
-  };
-
-  home-manager.users.jade = common.jade;
-
-  documentation.enable = false;
-
-  time.timeZone = common.timeZone;
-  environment.variables = {
-    NIX_HOST = hostname;
-    NIXDARWIN_CONFIG = configPath;
-  };
-  environment.darwinConfig = configPath;
-
-  users.users.jade = {
-    name = username;
-    home = "/Users/${username}";
-    openssh.authorizedKeys.keys = with common.pubkeys; [
-      atlantis
-      neverland
-      eldo
-      workbook
-    ];
-  };
-
-  system.stateVersion = 4;
-  ids.gids.nixbld = 350;
-  nix = common.nix // {
-    nixPath = [
-      "darwin=${common.nix-darwin}"
-      "darwin-config=${configPath}"
-    ];
-  };
-  services =
-    let
-      modelPath = name: "/opt/box/models/bartowski/agentica-org_DeepScaleR-1.5B-Preview-GGUF/${name}";
-    in
-    {
-      openssh.enable = true;
-      llama-server.servers = {
-        r1-7b = {
-          enable = true;
-          port = 8012;
-          model = modelPath "agentica-org_DeepScaleR-1.5B-Preview-Q4_0.gguf";
-          ngl = 99;
-        };
-      };
+  # ADDED the detailed powerManagement block:
+  powerManagement.enable = true; # Ensure the service is enabled
+  powerManagement.settings = {
+    AC = {
+      sleep = 0;
+      displaysleep = 0;
+      disksleep = 10;
+      hibernatemode = 3;
+      lidwake = 1;
     };
 
-}
+    home-manager.users.jade = common.jade;
+
+    documentation.enable = false;
+
+    time.timeZone = common.timeZone;
+    environment.variables = {
+      NIX_HOST = hostname;
+      NIXDARWIN_CONFIG = configPath;
+    };
+    environment.darwinConfig = configPath;
+
+    users.users.jade = {
+      name = username;
+      home = "/Users/${username}";
+      openssh.authorizedKeys.keys = with common.pubkeys; [
+        atlantis
+        neverland
+        eldo
+        workbook
+      ];
+    };
+
+    system.stateVersion = 4;
+    ids.gids.nixbld = 350;
+    nix = common.nix // {
+      nixPath = [
+        "darwin=${common.nix-darwin}"
+        "darwin-config=${configPath}"
+      ];
+    };
+    services =
+      let
+        modelPath = name: "/opt/box/models/bartowski/agentica-org_DeepScaleR-1.5B-Preview-GGUF/${name}";
+      in
+      {
+        openssh.enable = true;
+        llama-server.servers = {
+          r1-7b = {
+            enable = true;
+            port = 8012;
+            model = modelPath "agentica-org_DeepScaleR-1.5B-Preview-Q4_0.gguf";
+            ngl = 99;
+          };
+        };
+      };
+
+  }
