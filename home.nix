99f7a14fa3977a5877a4ef0bff40847afc84bc7a
jade fisher <fisherrjd@gmail.com>
2025-04-28 15:10:25 -0600

    fixes

--- home.nix
+++ home.nix
@@ -24,6 +24,8 @@ let
 in
 {
   imports = [
+    ./home_configurations/packages.nix
+    ./home_configurations/cobi.nix
     # Look more into these ex: optionalAttrs
     (pkgs.lib.optionalAttrs isLinux "${flake.inputs.vscode-server}/modules/vscode-server/home.nix")
   ];
