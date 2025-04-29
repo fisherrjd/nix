6235e8cc6865ac878edb3711de3fad4c4ab98a13
jade fisher <fisherrjd@gmail.com>
2025-04-28 18:54:16 -0600

    updates

--- colmena/hive.nix
+++ colmena/hive.nix
@@ -1,16 +1,11 @@
-{ inputs, ... }: # <-- This expects 'inputs' to be passed
-let
-  test = "test";
-in
 {
   meta = {
-    nixpkgs = inputs.nixpkgs; # <-- Accesses the passed 'inputs'
-    specialArgs = { inherit inputs; }; # <-- Also uses 'inputs'
+    nixpkgs = <nixpkgs>; # <-- Accesses the passed 'inputs'
   };
   nodes = {
     bifrost = { name, nodes, ... }: {
       imports = [
-        ./hosts/bifrost/configuration.nix
+        ../hosts/bifrost/configuration.nix
       ];
       deployment.targetHost = "bifrost";
       deployment.replaceUnknownProfiles = false;
