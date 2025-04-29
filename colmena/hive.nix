0d271c29dc5f0b62937633eac94df3a9c53da65c
jade fisher <fisherrjd@gmail.com>
2025-04-28 19:04:21 -0600

    remove nodes section

--- colmena/hive.nix
+++ colmena/hive.nix
@@ -2,16 +2,14 @@
   meta = {
     nixpkgs = <nixpkgs>; # <-- Accesses the passed 'inputs'
   };
-  nodes = {
-    bifrost = { name, nodes, ... }: {
-      imports = [
-        ../hosts/bifrost/configtest.nix
-      ];
-      deployment.targetHost = "104.236.220.8";
-      deployment.replaceUnknownProfiles = false;
-      deployment.allowLocalDeployment = false;
-      deployment.tags = [ "proxy" ];
+  bifrost = { name, nodes, ... }: {
+    imports = [
+      ../hosts/bifrost/configtest.nix
+    ];
+    deployment.targetHost = "104.236.220.8";
+    deployment.replaceUnknownProfiles = false;
+    deployment.allowLocalDeployment = false;
+    deployment.tags = [ "proxy" ];
 
-    };
   };
 }
