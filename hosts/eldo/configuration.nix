2efc5561d6e202cc2e04162221ff4afe38d63877
jade fisher <fisherrjd@gmail.com>
2025-04-29 15:22:57 -0600

    test new config

--- hosts/eldo/configuration.nix
+++ hosts/eldo/configuration.nix
@@ -134,13 +134,8 @@ in
         enable = true;
         ensureDatabases = [ "litellm" ];
         authentication = pkgs.lib.mkOverride 10 ''
-          #type database  DBuser  auth-method
-          local   all       all     trust
-          host    all       all     127.0.0.1/32 trust
-          host    all       all     0.0.0.0/0 trust
-          host    all            postgres         127.0.0.1/32           md5
-          host    all            postgres         ::1/128                md5
-          host    litellm        postgres         0.0.0.0/0              md5
+          #type database  DBuser  IP            auth-method
+          host  all       all     100.64.0.0/10 md5
         '';
       };
     };
