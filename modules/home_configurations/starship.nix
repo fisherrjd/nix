09ba79ed5a2f00d67805100aeeb824a6e07d53a6
jade fisher <fisherrjd@gmail.com>
2025-04-30 17:03:39 -0600

    new starship init

---
+++ modules/home_configurations/starship.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, lib, ... }:
+let promptChar = ">";
+in
+{
+  # Enable the starship program system-wide
+  programs.starship.enable = true;
+  programs.starship = {
+    settings = {
+      add_newline = false;
+      character = {
+        success_symbol = "[${promptChar}](bright-orange)";
+        error_symbol = "[${promptChar}](bright-orange)";
+      };
+      directory.style = "fg:#d442f5";
+      nix_shell = {
+        pure_msg = "";
+        impure_msg = "";
+        format = "via [$symbol$state($name)]($style) ";
+      };
+      kubernetes = {
+        disabled = false;
+        style = "fg:#326ce5";
+      };
+
+      nodejs = { symbol = "⬡ "; };
+      hostname = {
+        style = "bold bright-blue";
+      };
+      username = {
+        style_user = "bold fg:93";
+      };
+
+    };
+  };
+}
