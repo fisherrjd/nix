4ac4653fada2ce279fadd87fd8ba8eed29630fee
jade fisher <fisherrjd@gmail.com>
2025-04-29 18:49:44 -0600

    revert keys

--- hosts/secrets/secrets.nix
+++ hosts/secrets/secrets.nix
@@ -1,11 +1,15 @@
 let
   # TODO Abstract keys properly from common
   # common = import ../common.nix;
-  common = import ../common.nix;
+  neverland = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAX2/pWmcbFAPOSs1Vi4/xHRgFT+IDuWBUNGFyM0YlCh jade@neverland"; #home wsl on home desktop
+  eldo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbNhnkhqLCDhVYXTQXxuVYkPHnWSBFFmunVSk5ETnZj jade@eldo"; # old pc gone nix server
+  airbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILaWm9is28MRcO96v72pHvWQuZ+NiM0t3iFmC4mq3jsJ jade@airbook"; # m1 macbook air
+  dev = [ neverland eldo airbook ];
+
 in
 {
 
-  "litellm.age".publicKeys = common.pubkeys;
-  "openwebui.age".publicKeys = common.pubkeys;
-  "caddy.age".publicKeys = common.pubkeys;
+  "litellm.age".publicKeys = dev;
+  "openwebui.age".publicKeys = dev;
+  "caddy.age".publicKeys = dev;
 }
