1abf8a57b67feab3cc769236b54921e30b568738
Jade Fisher <fisherrjd@gmail.com>
2025-04-30 19:26:43 -0600

    re-add git

--- home.nix
+++ home.nix
@@ -27,6 +27,8 @@ in
     ./modules/home_configurations/packages.nix
     ./modules/home_configurations/starship.nix
     ./modules/home_configurations/cobi.nix
+    ./modules/home_configurations/git.nix
+
 
     # Look more into these ex: optionalAttrs
     (pkgs.lib.optionalAttrs isLinux "${flake.inputs.vscode-server}/modules/vscode-server/home.nix")
