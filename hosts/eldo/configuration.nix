359ca3f28471ab367350fc9838f0a5a6a3f3ffae
Jade Fisher <fisherrjd@gmail.com>
2025-05-02 13:07:16 -0600

    idk undo

--- hosts/eldo/configuration.nix
+++ hosts/eldo/configuration.nix
@@ -78,7 +78,6 @@ in
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
-  nixpkgs.config.allowUnfree = true;
   services =
     {
       ntfy-sh = {
