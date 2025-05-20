387b20db87ff3f0cdde19f3256faae33b7dda183
Jade Fisher <fisherrjd@gmail.com>
2025-05-20 12:11:41 -0600

    try to make it a list

--- flake.nix
+++ flake.nix
@@ -64,6 +64,7 @@
               specialArgs = { flake = self; machine-name = name; };
               modules = [
                 ./hosts/common_darwin.nix
+                { }
                 "${self.inputs.jacobi}/hosts/modules/darwin/llama-server.nix"
                 ./hosts/${name}/configuration.nix
               ];
