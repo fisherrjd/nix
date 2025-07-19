4f13b130a119cfd422abb9fb0311cd4d33d6d863
jade fisher <fisherrjd@gmail.com>
2025-07-19 17:53:09 -0600

    update port for llama cpp

--- hosts/airbook/configuration.nix
+++ hosts/airbook/configuration.nix
@@ -55,7 +55,7 @@ in
         Unsloth_Qwen3-4B-Q8_0 = {
           enable = true;
           package = pkgs.llama-cpp-latest;
-          port = 8012;
+          port = 6969;
           model = unsloth "Qwen3-4B-Q8_0.gguf";
           ngl = 99;
           extraFlags = ''--ctx-size 32768 --seed 420 --prio 2 --temp 0.6 --min-p 0.0 --top-k 20 --top-p 0.95'';
