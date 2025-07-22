d07634e761c7bf940b55d728170eb3f6d263069d
jade fisher <fisherrjd@gmail.com>
2025-07-21 20:41:48 -0600

    rename

--- mods/pog/datetime.nix
+++ mods/pog/datetime.nix
@@ -1,7 +1,7 @@
 # this file provides a simple pog script to print the current date and time
 final: prev: {
-  now = final.pog {
-    name = "now";
+  datetime = final.pog {
+    name = "datetime";
     description = "print the current date and time";
     flags = [ ];
     script = ''
