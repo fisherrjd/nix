a0be74a7e753ec5d6f07a961f100bd2862ee4223
jade fisher <fisherrjd@gmail.com>
2025-07-21 21:37:41 -0600

    remove overlay thing for passing pog to overlays

--- default.nix
+++ default.nix
@@ -8,9 +8,6 @@ import nixpkgs {
   inherit system;
   overlays = [
     # Ensure pog is available as final.pog in all overlays
-    (final: prev: {
-      pog = flake.inputs.pog.packages.${system}.default;
-    })
     (_: _: { nixpkgsRev = nixpkgs.rev; })
     (_: _: { jacobi = import flake.inputs.jacobi { inherit system; }; })
     (_: prev: { inherit (prev.jacobi) llama-cpp-latest; })
