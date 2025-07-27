6cf37590d17766aed71a32bade2e6b479f57b5cd
jade fisher <fisherrjd@gmail.com>
2025-07-27 10:15:08 -0600

    modify colmena hive definition

--- colmena/hive.nix
+++ colmena/hive.nix
@@ -1,8 +1,11 @@
+let
+  flake = import ../flake.nix;
+in
 {
   meta = {
     nixpkgs = <nixpkgs>;
   };
-  bifrost = { ... }: {
+  bifrost = {
     imports = [ ../hosts/bifrost/configuration.nix ];
     deployment.targetHost = "bifrost"; # Doesn't matter for 'build'
     deployment.targetUser = "jade";
