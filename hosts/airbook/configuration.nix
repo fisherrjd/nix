efbc63b1ad0246166cec1e379dac312aff452c10
jade fisher <fisherrjd@gmail.com>
2025-08-04 19:13:30 -0600

    updates to airbook

--- hosts/airbook/configuration.nix
+++ hosts/airbook/configuration.nix
@@ -23,7 +23,10 @@ in
     NIXDARWIN_CONFIG = configPath;
   };
   environment.darwinConfig = configPath;
-
+  environment.systemPackages = with pkgs; [
+    vim
+    vscode
+  ];
   users.users.jade = {
     name = username;
     home = "/Users/${username}";
@@ -52,13 +55,13 @@ in
     {
       openssh.enable = true;
       llama-server.servers = {
-        Qwen3-Coder-30B-A3B-Instruct-UD-Q2_K_XL = {
+        tencent_Hunyuan-7B-Instruct-Q8_0 = {
           enable = true;
           package = pkgs.llama-cpp-latest;
           port = 6969;
-          model = unsloth "Qwen3-Coder-30B-A3B-Instruct-UD-Q2_K_XL.gguf";
+          model = unsloth "tencent_Hunyuan-7B-Instruct-Q8_0.gguf";
           ngl = 99;
-          extraFlags = ''--ctx-size 8096 --seed 420 --prio 2 --temp 0.6 --min-p 0.0 --top-k 20 --top-p 0.95'';
+          extraFlags = ''--ctx-size 32768 --seed 420 --prio 2 --temp 0.6 --min-p 0.0 --top-k 20 --top-p 0.95'';
         };
       };
 
