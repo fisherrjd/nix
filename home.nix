ab3e76a52339be4f993636cfb50fbaa4815efe38
jade fisher <fisherrjd@gmail.com>
2026-01-04 18:54:19 -0700

    fix system rename warning

--- home.nix
+++ home.nix
@@ -154,7 +154,7 @@ in
             ]
           )
           # Secrets
-          flake.inputs.agenix.packages.${pkgs.system}.default
+          flake.inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
 
           #Packages NOT on Bifrost
           (lib.optionals notBifrost [
