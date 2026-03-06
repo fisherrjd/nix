ffef016b9b053677c5a39ea4d87b31bed54c800d
jade fisher <fisherrjd@gmail.com>
2026-03-05 18:36:01 -0700

    remove llm

--- hosts/airbook/configuration.nix
+++ hosts/airbook/configuration.nix
@@ -52,26 +52,26 @@ in
       bartowski = name: "/opt/box/models/bartowski/${name}";
     in
     {
-      openssh.enable = true;
-      llama-server.servers = {
-        Qwen2_5-VL-7B-Instruct-Q4_K_M = {
-          enable = true;
-          package = pkgs.llama-cpp-latest;
-          port = 6969;
-          model = unsloth "Qwen2.5-VL-7B-Instruct-Q4_K_M.gguf";
-          ngl = 99;
-          extraFlags = ''
-            --ctx-size 8192 
-            --seed 420 
-            --prio 2 
-            --temp 0.6 
-            --min-p 0.0 
-            --top-k 20 
-            --top-p 0.95 
-          '';
-        };
-      };
-      
+      # openssh.enable = true;
+      # llama-server.servers = {
+      #   Qwen2_5-VL-7B-Instruct-Q4_K_M = {
+      #     enable = true;
+      #     package = pkgs.llama-cpp-latest;
+      #     port = 6969;
+      #     model = unsloth "Qwen2.5-VL-7B-Instruct-Q4_K_M.gguf";
+      #     ngl = 99;
+      #     extraFlags = ''
+      #       --ctx-size 8192
+      #       --seed 420
+      #       --prio 2
+      #       --temp 0.6
+      #       --min-p 0.0
+      #       --top-k 20
+      #       --top-p 0.95
+      #     '';
+      #   };
+      # };
+
     };
   launchd.user.agents.caffeinate = {
     serviceConfig = {
@@ -97,6 +97,7 @@ in
 
     # 5. Casks (GUI apps)
     casks = [
+      "blackhole-2ch"
       "visual-studio-code"
       "firefox"
       "discord"
