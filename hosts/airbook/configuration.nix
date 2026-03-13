ad4ca9ca2bd71c976e70e5f8a76f291ebcc8a88a
jade fisher <fisherrjd@gmail.com>
2026-03-12 20:10:23 -0600

    remove llama-server

--- hosts/airbook/configuration.nix
+++ hosts/airbook/configuration.nix
@@ -53,16 +53,16 @@ in
     in
     {
       openssh.enable = true;
-      llama-server.servers = {
-        Qwen3_5-9B-Uncensored-HauhauCS-Aggressive-Q8_0 = {
-          enable = true;
-          package = pkgs.llama-cpp;
-          port = 6969;
-          model = unsloth "Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q8_0.gguf";
-          ngl = 0;
-          extraFlags = "--ctx-size 8192 --seed 420 --prio 2 --temp 0.6 --min-p 0.0 --top-k 20 --top-p 0.95";
-        };
-      };
+      # llama-server.servers = {
+      #   Qwen3_5-9B-Uncensored-HauhauCS-Aggressive-Q8_0 = {
+      #     enable = true;
+      #     package = pkgs.llama-cpp;
+      #     port = 6969;
+      #     model = unsloth "Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q8_0.gguf";
+      #     ngl = 0;
+      #     extraFlags = "--ctx-size 8192 --seed 420 --prio 2 --temp 0.6 --min-p 0.0 --top-k 20 --top-p 0.95";
+      #   };
+      # };
 
     };
   launchd.user.agents.caffeinate = {
