4b3654c22d4b843ecec14e799b8bb6ae489a7c4f
Hermes CI <hermes@ci.eldo>
2026-04-07 21:27:37 -0600

    update and change

--- hosts/eldo/configuration.nix
+++ hosts/eldo/configuration.nix
@@ -134,7 +134,7 @@ in
 
         extraPackages = [
           flake.inputs.hermes-agent.packages.${pkgs.stdenv.hostPlatform.system}.default
-        ] ++ (with pkgs; [ git jq curl ]);
+        ] ++ (with pkgs; [ git jq curl gh ]);
         serviceOverrides = {
           DynamicUser = false;
           User = "jade";
