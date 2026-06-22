d9824976018c7189a435daf8604bf6ad1c677b9e
Jade <92651075+fisherrjd@users.noreply.github.com>
2026-06-22 12:03:33 -0600

    new machine

---
+++ hosts/sinchbook/configuration.nix
@@ -0,0 +1,61 @@
+{ lib, config, flake, machine-name, pkgs, ... }:
+let
+  # inherit (lib.attrsets) mapAttrs' nameValuePair;
+  inherit (lib) mkDefault;
+  hostname = "workbook";
+  common = import ../common.nix { inherit config flake machine-name pkgs username; };
+  username = "jadfis";
+  configPath = "/Users/${username}/cfg/hosts/${hostname}/configuration.nix";
+in
+{
+  imports = [
+    "${common.home-manager}/nix-darwin"
+  ];
+
+  home-manager.users.jadfis = common.jade;
+
+  documentation.enable = false;
+  time.timeZone = common.timeZone;
+  environment.variables = {
+    NIX_HOST = hostname;
+    NIXDARWIN_CONFIG = configPath;
+  };
+  environment.darwinConfig = configPath;
+  users.users.jadfis = {
+    name = username;
+    home = "/Users/${username}";
+    openssh.authorizedKeys.keys = with common.pubkeys; [
+      atlantis
+      eldo
+    ];
+  };
+  system.primaryUser = mkDefault username;
+  system.stateVersion = 4;
+  ids.gids.nixbld = 350;
+
+  nix = common.nix // {
+    nixPath = [
+      "nixpkgs=${flake.inputs.nixpkgs}"
+      "darwin=${common.nix-darwin}"
+      "darwin-config=${configPath}"
+    ];
+    extraOptions = ''
+      max-jobs = auto
+      narinfo-cache-negative-ttl = 10
+      extra-experimental-features = nix-command flakes
+      extra-substituters = https://fisherrjd.cachix.org
+      extra-trusted-public-keys = fisherrjd.cachix.org-1:21bdYeKCoWN19OGUDTGU41o60gnEsLHY5+tIpEq7w+A=
+    '';
+  };
+  services.openssh.enable = true;
+
+  homebrew = {
+    enable = true;
+    taps = [
+    ];
+    brews = [
+    ];
+    casks = [
+    ];
+  };
+}
